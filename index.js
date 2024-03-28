7a1b1da1429f0b0f2af1875add391f64ae738914
Andrew Rea <andrew.s.rea@gmail.com>
2024-03-28 14:50:32 -0700

    Adding JS to play audio clips and add  class to buttons

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 window.addEventListener('keydown', function(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audio);
-})
+  const audio = document.querySelector(`audio[data-event-key="${e.key}"]`);
+  const button = document.querySelector(`.button[data-event-key="${e.key}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  button.classList.add('playing');
+});
