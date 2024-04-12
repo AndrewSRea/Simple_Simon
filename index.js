9c7f9602e3254dd3aa4be7153bb17fc5b20f8978
Andrew Rea <andrew.s.rea@gmail.com>
2024-04-12 10:50:26 -0700

    Adding a JS function to generate a randomly chosen color string and push it into an array

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* *** FINAL PRODUCT -- GITHUB VERSION *** */
+
 window.addEventListener('keydown', function(e) {
   const audio = document.querySelector(`audio[data-event-key="${e.key}"]`);
   const button = document.querySelector(`.button[data-event-key="${e.key}"]`);
@@ -6,3 +8,20 @@ window.addEventListener('keydown', function(e) {
   audio.play();
   button.classList.add('playing');
 });
+
+// typing `genRandomNum()` in the console will push a randomly selected color into the
+// `randColors` array (probably should have this array outside this function)
+function genRandomColor() {
+  let randColors = []
+  let randomNum = Math.floor(Math.random() * 4) + 1;
+  if (randomNum === 1) {
+     randColors.push("green");
+  } else if (randomNum === 2) {
+     randColors.push("yellow")
+  } else if (randomNum === 3) {
+     randColors.push("red") 
+  } else {
+     randColors.push("blue")
+  }
+  console.log(randColors);
+}
